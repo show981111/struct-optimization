b1eeb2682a3099f4ea37e9e8ef5a8f89a2e39bbf
Yong Seung Lee <leeyongs@eecs583a.eecs.umich.edu>
2023-11-26 01:11:56 -0500

    clean

--- hw2pass/hw2pass.cpp
+++ hw2pass/hw2pass.cpp
@@ -1,45 +1,7 @@
-//===-- Frequent Path Loop Invariant Code Motion Pass --------------------===//
+//===--------------------------- Struct Optimization --------------------===//
 //
-//                     The LLVM Compiler Infrastructure
-//
-// This file is distributed under the University of Illinois Open Source
-// License. See LICENSE.TXT for details.
 //
 //===---------------------------------------------------------------------===//
-//
-// EECS583 F23 - This pass can be used as a template for your FPLICM homework
-//               assignment.
-//               The passes get registered as "fplicm-correctness" and
-//               "fplicm-performance".
-//
-//
-////===-------------------------------------------------------------------===//
-// #include "llvm/Analysis/BlockFrequencyInfo.h"
-// #include "llvm/Analysis/BranchProbabilityInfo.h"
-// #include "llvm/Analysis/LoopInfo.h"
-// #include "llvm/Analysis/LoopIterator.h"
-// #include "llvm/Analysis/LoopPass.h"
-// #include "llvm/IR/CFG.h"
-// #include "llvm/IR/Instructions.h"
-// #include "llvm/IR/LLVMContext.h"
-// #include "llvm/IR/PassManager.h"
-// #include "llvm/Passes/PassBuilder.h"
-// #include "llvm/Passes/PassPlugin.h"
-// #include "llvm/Support/Debug.h"
-// #include "llvm/Support/raw_ostream.h"
-// #include "llvm/Transforms/Scalar/LoopPassManager.h"
-// #include "llvm/Transforms/Utils/BasicBlockUtils.h"
-// #include "llvm/Transforms/Utils/LoopUtils.h"
-// #include "llvm/Analysis/DominanceFrontier.h"
-// #include "llvm/Pass.h"
-// #include "llvm/IR/Function.h"
-// #include "llvm/IR/Instruction.h"
-// #include "llvm/IR/Instructions.h"
-// #include "llvm/Support/raw_ostream.h"
-// #include "llvm/IR/DerivedTypes.h"
-// #include "llvm/IR/Module.h"
-// #include "llvm/Support/raw_ostream.h"
-// #include "llvm/IR/IRBuilder.h"
 #include "StructOpt.h"
 
 #include <string>
@@ -110,17 +72,6 @@ extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK llvmGetPassPluginIn
       LLVM_PLUGIN_API_VERSION, "HW2Pass", "v0.1",
       [](PassBuilder &PB)
       {
-        // PB.registerPipelineParsingCallback(
-        //     [](StringRef Name, FunctionPassManager &FPM,
-        //        ArrayRef<PassBuilder::PipelineElement>)
-        //     {
-        //       if (Name == "fplicm-correctness")
-        //       {
-        //         FPM.addPass(HW2CorrectnessPass());
-        //         return true;
-        //       }
-        //       return false;
-        //     });
         PB.registerPipelineParsingCallback(
             [](StringRef Name, ModulePassManager &MPM,
                ...)
