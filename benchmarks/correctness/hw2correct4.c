6ec8c78532e035ddab755429112fecd1453231c9
Yong Seung Lee <yongseunglee@Yongs-MacBook-Air.local>
2023-11-26 17:02:09 -0500

    clean up

--- benchmarks/correctness/hw2correct4.c
+++ benchmarks/correctness/hw2correct4.c
@@ -6,23 +6,42 @@ typedef struct
 	int b;
 	int e[100];
 	long c;
-	char d;
+	int d;
 	long f;
 	long g;
 	long h;
 } Test;
 
+typedef struct
+{
+	int a;
+	int b;
+	int e[100];
+	long c;
+	char d;
+	long f;
+	long g;
+	long h;
+} Hello;
+
 int main()
 {
-	Test *t = (Test *)malloc(sizeof(Test));
-
-	for (int i = 0; i < 10; i++)
+	Test t;
+	Hello h;
+	t.a = 0;
+	t.d = 0;
+	t.f = 0;
+	h.a = 0;
+	h.b = 0;
+	for (int i = 0; i < 50; i++)
 	{
-		t->a = 10;		 // => arr_1[i]->a = 10;
-		t->d = t->a + 3; // => arr_2[i]->c = arr_1[i]->a + 3;
-		t->e[3] = 12;
+		t.a = t.a + 10; // => arr_1[i]->a = 10;
+		t.d = t.a + 3;	// => arr_2[i]->c = arr_1[i]->a + 3;
+		h.f = t.a + t.d;
+		h.a = t.a + h.f;
+		h.b = h.a + 1;
 	}
 
-	printf("%d %d %d", t->a, t->d, t->e[3]);
+	printf("%d %d %d %d %d\n", t.a, t.d, t.f, h.a, h.b);
 	return 0;
 }
--- compare.sh
+++ compare.sh
@@ -1,7 +1,10 @@
 cd benchmarks/correctness/
 
 rm -rf cachegrind.out.*
-echo "=========== UNOPTIMIZED ==========="
-valgrind --tool=cachegrind ./hw2correct${1}_no_fplicm
-echo "=========== OPTIMIZED ==========="
-valgrind --tool=cachegrind ./hw2correct${1}_fplicm
+echo "=========== UNOPTIMIZED (Original) ===========\n\n" > ../results/benchmark${1}.txt
+valgrind --tool=cachegrind ./hw2correct${1}_no_fplicm 2>> ../results/benchmark${1}.txt
+echo "\n\n=========== UNOPTIMIZED (Profile) ===========\n\n" >> ../results/benchmark${1}.txt
+valgrind --tool=cachegrind ./hw2correct${1}_prof 2>> ../results/benchmark${1}.txt
+echo "\n\n=========== OPTIMIZED ===========\n\n" >> ../results/benchmark${1}.txt
+valgrind --tool=cachegrind ./hw2correct${1}_fplicm 2>> ../results/benchmark${1}.txt
+rm -rf cachegrind.out.*
